b8b5f2fec95af41ab214190b156335ddb7f736ce
Juliayn Magpie <juliayn.magpie@gmail.com>
2024-10-26 21:50:30 -0600

    homework week 9

---
+++ homework 9/sketch.js
@@ -0,0 +1,56 @@
+function setup()
+{
+createCanvas(500, 600);
+}
+function draw()
+{
+background(290, 60, 90);
+
+fill('#fae');
+circle(250, 260, 450);
+
+fill(500, 520, 200)
+ellipse(150, 200, 115, 80)
+
+fill(500, 520, 200)
+ellipse(350, 200, 115, 80)
+
+fill('rgb(100%, 0%, 10%)');
+circle(350, 200, 70)
+
+fill(50, 50, 0)
+circle(150,200, 70)
+
+// Top-left.
+point(205, 260);
+
+// Top-right.
+point(100, 415);
+
+fill(220, 100, 300)
+triangle(250, 180, 300, 320, 200, 320);
+
+fill(200, 50, 40)
+ellipse(250, 400, 150, 80)
+
+line(180, 400, 320, 400);
+
+stroke('black');
+  strokeWeight(5);
+
+line(16, 590, 26, 230);
+  
+line(478, 590, 474, 230);
+
+fill(60, 10, 90)
+rect(200, 485, 100, 112);
+
+fill(500, 10, 400)
+triangle(250, 580, 300, 485, 200, 485);
+
+fill(155, 204, 0)
+text('Portrait of Feather', 20, 40);
+
+fill(155, 204, 0)
+text('Juliayn Magpie', 390, 580);
+}
